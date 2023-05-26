a4e424af7cf84e8eacbcc31aac693fd10e963a9d
dungnt2khust <eddieonthecode@gmail.com>
2023-05-26 08:46:02 +0700

    change

--- projects/angular-resizable-pane/src/lib/angular-resizable-pane.component.ts
+++ projects/angular-resizable-pane/src/lib/angular-resizable-pane.component.ts
@@ -2,13 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'angular-resizable-pane',
-  template: `
-    <p>
-      angular-resizable-pane works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './angular-resizable-pane.html',
+  styleUrls: ['./angular-resizable-pane.scss']
 })
 export class AngularResizablePaneComponent {
 
